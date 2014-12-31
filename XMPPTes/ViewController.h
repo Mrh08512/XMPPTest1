ec8722b690314cf4d32ddc268a1b259b75775a0a
MRH-MAC <apple@MrH-Pro.local>
2014-12-31 13:36:42 +0800

    Release 1.0.0.

---
+++ XMPPTes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMPPTest1
+//
+//  Created by MRH-MAC on 14/12/29.
+//  Copyright (c) 2014年 MRH-MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XMPPTes/ViewController.m
@@ -0,0 +1,36 @@
+//
+//  ViewController.m
+//  XMPPTest1
+//
+//  Created by MRH-MAC on 14/12/29.
+//  Copyright (c) 2014年 MRH-MAC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+ 
+    // Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)actionSendMes:(id)sender
+{
+    
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
